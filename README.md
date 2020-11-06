55dbee24c57b58c420ffa147e0a2d7b59081e356
Abhilash M Nair <abhilashmnair20@gmail.com>
2020-11-06 19:34:26 +0530

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Instagram-Spammer
- A simple Instagram message spamming with Python
+A simple Instagram message spamming with Python
+
+Steps :
+
+1) Install Selenium : `pip install -U selenium`
+
+2) Downlaod the Chrome webdriver from [here](https://chromedriver.chromium.org/downloads).
+   (Extract the zip and replace the path with the .exe file's path )
+
+3) Run the code : `py insta-spam.py`
+
+4) Provide your username & password. (For login purpose only)
+
+5) Provide your friend's username and the message to send. Crazy!
+
+How long it works? Well...till your PC is thrashed or it's processor fries!!!
+
+**Enjoy**
+
+*This is not intended to cause any trouble. Don't blame me if your homie reported you as spam or your girlfriend dumped you* :)
---
+++ insta-spam.py
@@ -0,0 +1,33 @@
+import time
+from selenium import webdriver
+
+driver = webdriver.Chrome('C:\webdrivers\chromedriver.exe')
+print("Your credentials are not stored or passed anywhere.\n---------------------------------------------------")
+username = input("Enter username : ")
+password = input("Enter your password : ")
+victimName = input("Enter the victim's username : ")
+message = input("Enter the message to send : ")
+
+driver.implicitly_wait(10)
+driver.get('https://instagram.com/direct/inbox')
+
+#Provide username and password and clicks proceed
+driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[1]/div/label/input').send_keys(username)
+driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[2]/div/label/input').send_keys(password)
+driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[3]/button').click()
+
+#Directs to messaging window by dismissing pop-ups
+driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div/div/div/button').click()
+driver.find_element_by_xpath('/html/body/div[4]/div/div/div/div[3]/button[2]').click()
+
+#Clicks search bar and find the person. Then enter the chat
+driver.find_element_by_xpath('//*[@id="react-root"]/section/div/div[2]/div/div/div[1]/div[1]/div/div[3]/button').click()
+driver.find_element_by_xpath('/html/body/div[5]/div/div/div[2]/div[1]/div/div[2]/input').send_keys(victimName)
+driver.find_element_by_xpath('/html/body/div[5]/div/div/div[2]/div[2]/div[1]/div/div[3]/button').click()
+time.sleep(1)
+driver.find_element_by_xpath('/html/body/div[5]/div/div/div[1]/div/div[2]/div/button').click()
+time.sleep(3)
+
+while(True):
+    driver.find_element_by_xpath('//*[@id="react-root"]/section/div/div[2]/div/div/div[2]/div[2]/div/div[2]/div/div/div[2]/textarea').send_keys(message)
+    driver.find_element_by_xpath('//*[@id="react-root"]/section/div/div[2]/div/div/div[2]/div[2]/div/div[2]/div/div/div[3]/button').click()
